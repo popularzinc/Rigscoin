5bbde4de82688f8104c3c0a9f95471610c1b2b6a
popularzinc <93499654+popularzinc@users.noreply.github.com>
2021-12-18 13:18:04 -0600

    Add files via upload

--- examples.py
+++ examples.py
@@ -1,7 +1,19 @@
 import Rigs
 
-key = Rigs.Key()
-key.Generate()
-key.Save('Miner')
+key = Rigs.Key()                # Opens Key
+key.Generate()                  # Generates Public/Private key pair
+key.Save('example')             # Saves keys and address to folder "example"
+# Example Addr: 7ZAJEBDFLr8FYmBqKA8G5L2MMeRiJKPHQJTiircmuRoS
 
-#key.Genysis(key.address,1000)
+key.Genysis(key.address,1000)
+
+key = Rigs.Key()                # Opens Key
+key.GenerateStealth()           # Generates Stealth address
+key.Save('stealth-example')     # Saves keys and address to folder "stealth-example"
+# Example Stealth Addr: iKx1CJLa5Dp8mYfmcLAHMf7EjxSFhWEpxcLq9wwqTqhFMP9zHggz74ffaXC2Pwcvid3CYJ7xtNU9h89bouR5SZ6ZnoVqVdXHmX
+
+key = Rigs.Key()                # Opens Key
+key.Load('example')             # Load keys from folder "example"
+
+key.Send('7ZAJEBDFLr8FYmBqKA8G5L2MMeRiJKPHQJTiircmuRoS',100)        # Send transation
+key.Send('iKx1CJLa5Dp8mYfmcLAHMf7EjxSFhWEpxcLq9wwqTqhFMP9zHggz74ffaXC2Pwcvid3CYJ7xtNU9h89bouR5SZ6ZnoVqVdXHmX',100)      # Send transaction to stealth address
