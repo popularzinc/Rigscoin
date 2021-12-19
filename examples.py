00515a5649fe62a2c433b66fb883643ac70702c1
popularzinc <93499654+popularzinc@users.noreply.github.com>
2021-12-18 19:57:21 -0600

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,19 +1,29 @@
+from Rigs import Network
 import Rigs
 
-key = Rigs.Key()                # Opens Key
-key.Generate()                  # Generates Public/Private key pair
-key.Save('example')             # Saves keys and address to folder "example"
-# Example Addr: 7ZAJEBDFLr8FYmBqKA8G5L2MMeRiJKPHQJTiircmuRoS
+Network = Network.Network('83.867.235.12') # leave blank for automatic I.E: Network = Network.Network()
 
+# generate and save key to file folder "example"
+key = Rigs.Key()
+key.Generate()
+key.Save('example')
+
+# load key from folder "example"
+key = Rigs.Key()
+key.Load('example')
 key.Genysis(key.address,1000)
 
-key = Rigs.Key()                # Opens Key
-key.GenerateStealth()           # Generates Stealth address
-key.Save('stealth-example')     # Saves keys and address to folder "stealth-example"
-# Example Stealth Addr: iKx1CJLa5Dp8mYfmcLAHMf7EjxSFhWEpxcLq9wwqTqhFMP9zHggz74ffaXC2Pwcvid3CYJ7xtNU9h89bouR5SZ6ZnoVqVdXHmX
+# ping network to test connection
+print(Network.Ping())
 
-key = Rigs.Key()                # Opens Key
-key.Load('example')             # Load keys from folder "example"
+# generate a transaction
+transaction = key.GenTransaction('7ZAJEBDFLr8FYmBqKA8G5L2MMeRiJKPHQJTiircmuRoS',100)
+# send transaction to network
+Network.SendTransaction(transaction)
 
-key.Send('7ZAJEBDFLr8FYmBqKA8G5L2MMeRiJKPHQJTiircmuRoS',100)        # Send transation
-key.Send('iKx1CJLa5Dp8mYfmcLAHMf7EjxSFhWEpxcLq9wwqTqhFMP9zHggz74ffaXC2Pwcvid3CYJ7xtNU9h89bouR5SZ6ZnoVqVdXHmX',100)      # Send transaction to stealth address
+# generate stealth transaction
+data,data1 = key.GenStealthTransaction('iKx1CJLa5Dp8mYfmcLAHMf7EjxSFhWEpxcLq9wwqTqhFMP9zHggz74ffaXC2Pwcvid3CYJ7xtNU9h89bouR5SZ6ZnoVqVdXHmX',100)
+# send first transaction
+Network.SendTransaction(data)
+# send second transaction
+Network.SendTransaction(data1)
